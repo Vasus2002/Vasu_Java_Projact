504e4a4978b51a9529592988422b10c0c525bf43
Vasus2002 <mcac0931@gmail.com>
2024-05-30 10:22:11 +0530

    Add files via upload

---
+++ CircleArea_final.java
@@ -0,0 +1,12 @@
+package employeeData;
+
+public class CircleArea {
+	public static void main(String[] args) {
+        final double PI = 3.14;
+        double radius = 5.0;         
+
+        double area = PI * radius * radius;
+        
+        System.out.println("circle is: " + area);
+    }
+}
